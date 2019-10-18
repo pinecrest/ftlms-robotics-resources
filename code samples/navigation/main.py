8144063faee138b2c89e4844e06d70912d6f64ca
Sean Tibor <40579452+seantibor@users.noreply.github.com>
2019-10-18 15:34:44 -0400

    added drive_distance function example

--- code samples/navigation/main.py
+++ code samples/navigation/main.py
@@ -8,5 +8,19 @@ from pybricks.parameters import (Port, Stop, Direction, Button, Color,
 from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
 
-# Write your program here
-brick.sound.beep()
+r_motor = Motor(Port.C)
+l_motor = Motor(Port.B)
+# measure these for each robot - this could change based on the robot design
+wheel_diameter = 56
+axle_track = 114
+
+driver = DriveBase(l_motor, r_motor, wheel_diameter, axle_track) 
+
+def drive_distance(distance_mm):
+    speed = 300.0 # mm per second
+    ttd = distance_mm / speed * 1000
+    driver.drive(speed, 0)
+    wait(ttd)
+    driver.stop(Stop.BRAKE)
+
+drive_distance(1250)
