6bd9db89e215e21ed40e8bce335e5c58e4ce8d06
Sean Tibor <40579452+seantibor@users.noreply.github.com>
2019-12-06 15:34:10 -0500

    updated drive_distance example code
    Co-authored-by: FM2005 <fiona.mateo@pinecrest.edu>
    Co-authored-by: BHamaway <bryce.hamaway@pinecrest.edu>

--- code samples/navigation/main.py
+++ code samples/navigation/main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env pybricks-micropython
+ #!/usr/bin/env pybricks-micropython
 
 from pybricks import ev3brick as brick
 from pybricks.ev3devices import (
@@ -33,7 +33,7 @@ axle_track = 114
 driver = DriveBase(l_motor, r_motor, wheel_diameter, axle_track)
 
 
-def drive_distance(distance_mm):
+def drive_distance(distance_mm, speed_mm_s=300):
     """
     This function uses the DriveBase object to move the robot 
     forward in a straight line for a given distance in millimeters. 
@@ -41,16 +41,30 @@ def drive_distance(distance_mm):
     This does NOT use the gyro or any sensors other than what is 
     included in DriveBase.
 
+    We've also added ramp parameters to smoothly increase the motor speed
+    and prevent jerky starts.
+
     params:
     distance_mm - the distance to travel in millimeters
 
     """
-    speed = 300.0  # mm per second
-    ttd = distance_mm / speed * 1000
-    driver.drive(speed, 0)
+    ramp_interval = 1 # this is the time in seconds to ramp up
+    ramp_steps = 10
+
+    for i in range(ramp_steps):
+        driver.drive(speed_mm_s / (i + 1)) 
+        wait(ramp_interval * 1000 / ramp_steps)
+    distance_mm -= (speed_mm_s * ramp_interval / 2)
+    if distance_mm <= 0:
+        driver.stop(Stop.BRAKE)
+        return
+    ttd = distance_mm / speed_mm_s * 1000
+    driver.drive(speed_mm_s, 0)
     wait(ttd)
     driver.stop(Stop.BRAKE)
 
+def turn_angle(angle_deg, speed_deg_s=180) 
+    pass
 
 if __name__ == "__main__":
     drive_distance(1250)
